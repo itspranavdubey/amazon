741a053a9702236e44719a3500216fe6ef98ec08
itspranavdubey <stsnetking@gmail.com>
2025-06-23 10:19:52 +0000

    Updated the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,8 @@ hello
 this file contain deployment
 
 updated this in development branch
+
+=====Cherry Pick Demo=====
+Hello Updated this file for cherry pick demo
+this file is deploy.sh
+thank you
